e89dfcdc3af0478d560fb754b1390be7b0bb9e18
Guilherme Bini <77159678+Bini19@users.noreply.github.com>
2021-03-08 20:39:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Identidade_n_Por_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho;
+
+            Console.WriteLine("Informe o tamanho da Matrix Identidade: ");
+            tamanho = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n Matrix Identidade\n\n");
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
